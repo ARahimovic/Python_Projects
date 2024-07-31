3842bd173eaaf63d9e476a9b81eb6e696383e79c
Abderrahim Asselah (Nokia) <abderrahim.asselah@nokia.com>
2024-07-31 10:59:19 +0300

    Fix the issue in sudoku_generator
    - Fix the algorithm used and rely on backtracking to solve it
    - reset the change made to the sudoku_solver as it was not the issue

--- Sudoku/sudoku_generator.py
+++ Sudoku/sudoku_generator.py
@@ -11,47 +11,33 @@ class SudokuGenerator :
             count = 60
         for _ in range(count):
             self.sudoku.set_value(random.randint(0,8),random.randint(0,8),0)
-    
-    # def isValuePossible(self,x,y,val):
-    #     #check if value is valid
-    #     for i in range(self.sudoku.row_size):
-    #         #check same row
-    #         if self.sudoku.get_value(x,i) == val:
-    #             return False
-    #         #check same column
-    #         if self.sudoku.get_value(i,y) == val: 
-    #             return False
-    #         #check same 3x3 grid
-    #         if self.sudoku.get_value((x//3)*3 + i//3, ((y//3)*3 + i%3)) == val:
-    #             return False
-    #     return True
 
-    def generate_full_sudoku(self):
-        for row in range(self.sudoku.row_size):
-            repeat_counter = 0
-            column = 0
-            while column < self.sudoku.column_size:
-                while repeat_counter < 9:
-                        #generate random value
-                        val = random.randint(1,9)
-                        if (self.sudoku.isValuePossible(row,column,val)):
-                        #if value is valid, I assign  it to the cell and move to the next column
-                            self.sudoku.set_value(row, column, val)
-                            column += 1
-                            repeat_counter = 0
-                            break                    
-                        else :    
-                            #if value is not valid, I repeat the process and get another random value and check again
-                            repeat_counter += 1
-                            #if I have tried 9 times and still no valid value, I reset the row matrix and start again
-                            if(repeat_counter >= 9):
-                                #reset row matrix
-                                self.sudoku.matrix[row] = [0 for i in range(9)]
-                                repeat_counter = 0
-                                column = 0 
+
+    def _generate_full_sudoku(self):
+        for i in range (self.sudoku.row_size):
+            for j in range(self.sudoku.column_size):
+                #if there is empty cell, sudoku not complete, we fill it 
+                if self.sudoku.get_value(i,j) == 0:
+                    validNumbers = list(range(1, 10))
+                    random.shuffle(validNumbers)
+                    #we get a random number from 1 to 9
+                    for val in validNumbers:
+                        #we check if we can enter the value in the cell
+                        if self.sudoku.isValuePossible(i,j,val):
+                            #if true, we enter the value in the cell
+                            self.sudoku.set_value(i,j,val)
+                            #we recursively call the function to find the next empty cell and fill it / or end the sudoku if complete
+                            if self._generate_full_sudoku():
+                                return True
+                            #if filling the sudoku fails , reset the cell and try next number
+                            self.sudoku.set_value(i,j,0)
+                    #if no value is possible, return False
+                    return False
+        #if no empty cell is found, the sudoku is complete
+        return True
     
     def generate_sudoku(self):
-        self.generate_full_sudoku()
+        self._generate_full_sudoku()
         self.empty_cells(40)
         return self.sudoku
 
