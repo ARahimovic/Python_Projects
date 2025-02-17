dba9e2a0b287bbf56bd421f37e91df83655830e4
unknown <rahimasselah@gmail.com>
2025-02-17 19:51:16 +0200

    Reorganise Repos:
    - Python projects will have all the python personal projects

--- python_projects/Madlibs.py
+++
@@ -1,8 +0,0 @@
-protaganist = input("protaganist : ").strip()
-antagonist = input("antagonist : ").strip()
-weapon = input("weapon : ").strip()
-
-
-madlib = f"{protaganist} was heavily wonded, one knee on the floor, he was panthing heavily, {antagonist} was coming slowly towarsd him, his laugh louder and louder as he approach a weakend {protaganist}, as he was wihtin his {weapon}, he lifted up to deal the final blow, but {protaganist} muster up all his remaing strenght in a last roll to evade the attack while dealing a beautiful counter "
-
-print(madlib)
